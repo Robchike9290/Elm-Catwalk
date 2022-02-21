73c33925b88a9724de40a60c05fb32a5a36bf417
Robert Czajka <robchike9290@gmail.com>
2022-02-21 10:10:55 -0600

    Pull ratings and reviews section

--- client/App.js
+++ client/App.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-=======
 import React, { useContext, useState } from "react";
 import Token from '../config.js'
 import RatingsReviewsSection from './Ratings&Reviews/RatingsReviewsSection.jsx'
 import axios from 'axios';
->>>>>>> c6cc1e04ace9a5c04fe4560603ce8c0dda8a3730
 import Overview from "./Overview/Overview.jsx";
 import RelatedItemsAndComparison from "./RelatedProductsList/relatedItemsAndComparison.jsx";
 import { AppContext } from "./context.js";
@@ -20,9 +16,7 @@ const AppProvider = (props) => {
    const [currentStyleThumbnails, setCurrentStyleThumbnails] = useState([]);
    const [styleList, setStyleList] = useState([]);
    const [user, setUser] = useState({});
-<<<<<<< HEAD
    const [relatedProductsInfo, setRelatedProductsInfo] = useState([]);
-=======
 
    // ! Cheryl
    const [recommend, setRecommend] = useState(false);
@@ -38,7 +32,6 @@ const AppProvider = (props) => {
    const [productresults, setProductResults] = useState([])
    const [average, setAverage] = useState(0);
    // ! Cheryl
->>>>>>> c6cc1e04ace9a5c04fe4560603ce8c0dda8a3730
 
    return (
       <AppContext.Provider value={{
@@ -50,10 +43,8 @@ const AppProvider = (props) => {
          currentStylePhoto, setCurrentStylePhoto,
          currentStyleThumbnails, setCurrentStyleThumbnails,
          styleList, setStyleList,
-<<<<<<< HEAD
-         currentStar, setCurrentStar,
+         user, setUser,
          relatedProductsInfo, setRelatedProductsInfo,
-=======
 
          // ! Cheryl
          recommend, setRecommend,
@@ -69,7 +60,6 @@ const AppProvider = (props) => {
          productresults, setProductResults,
          average, setAverage,
          // ! Cheryl
->>>>>>> c6cc1e04ace9a5c04fe4560603ce8c0dda8a3730
          }}>
          {props.children}
       </AppContext.Provider>
@@ -80,11 +70,8 @@ function App() {
    return (
       <AppProvider>
          <Overview />
-<<<<<<< HEAD
          <RelatedItemsAndComparison/>
-=======
          <RatingsReviewsSection/>
->>>>>>> c6cc1e04ace9a5c04fe4560603ce8c0dda8a3730
       </AppProvider>
    )
 }
